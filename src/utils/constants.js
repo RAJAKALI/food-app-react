e58dfc2bbb133fc91217f55ba3f19813a825a427
kali <kalirajaylm@gmail.com>
2024-11-08 22:01:46 +0530

    cors error fixed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 export const LOGO ="https://img.freepik.com/premium-vector/beautiful-unique-food-restaurant-company-logo-design_981150-2182.jpg";
 export const IMG_CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-export const RES_INFO_API="https://cors-anywhere.herokuapp.com/https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.8167303&lng=77.6974369&restaurantId=";  
+export const RES_INFO_API="https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.8167303&lng=77.6974369&restaurantId=";  
